8db5331aea9643e5a328b0fe8829caaeb27a95c3
gnardini <gonzalo.nardini.dev@gmail.com>
2016-08-18 00:49:55 -0300

    Adds ball taht can be moved and basic walls

---
+++ Assets/Scripts/ScreenResizer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenResizer : MonoBehaviour {
+
+	void Start () {
+        GetComponent<Camera>().orthographicSize = Screen.height / 2f;        	
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## TurnSoccer
+
+Turn-based game where the players can be given impulse and push the ball to the opponent's goal.
